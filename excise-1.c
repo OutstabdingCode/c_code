0ea9799951b616517b196b998015ddb3a84638ec
OutstandingCode <1615324520@qq.com>
2023-09-16 18:02:14 +0800

    测试上传

---
+++ excise-1.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////¼ÆËãnµÄ½×³Ë
+//int main()
+//{
+//	int flag = 0;
+//	int i = 0;
+//	int sum = 1;
+//	printf("enter a number :");
+//	scanf("%d", &flag);
+//	for (i = 1; i <= flag; i++)
+//	{
+//		sum *= i;
+//	}
+//	printf("result:%d\n", sum);
+//	return 0;
+//}
